43a4703c45f625fd13abfffd66b19b31b337832c
ProbablyRaging <probablyraging@gmail.com>
2025-03-25 11:54:13 +1100

    Added Italian, Russian and Ukrainian languages
    Co-authored-by: Jesewe <Jesewe@users.noreply.github.com>
    Co-authored-by: Bl0ck154 <Bl0ck154@users.noreply.github.com>

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -4,10 +4,16 @@ import { initReactI18next } from 'react-i18next';
 
 import translationDE from '@/i18n/locales/de/translation.json';
 import translationEN from '@/i18n/locales/en/translation.json';
+import translationIT from '@/i18n/locales/it/translation.json';
+import translationRU from '@/i18n/locales/ru/translation.json';
+import translationUK from '@/i18n/locales/uk/translation.json';
 
 const resources = {
     de: { translation: translationDE },
     en: { translation: translationEN },
+    it: { translation: translationIT },
+    ru: { translation: translationRU },
+    uk: { translation: translationUK },
 };
 
 i18n
