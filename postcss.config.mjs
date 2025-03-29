c0cbf95d0b4acd5b29f011ca93cbb6005d84d7db
zevnda <zevnda@gmail.com>
2025-03-30 05:25:13 +1100

    build(postcss): add cssnano (#224)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        cssnano: {
+            preset: ['default', { discardComments: { removeAll: true } }],
+        }
+    },
 };
 
 export default config;
