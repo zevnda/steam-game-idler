fe19f5a4878f44262224257cdb50535de8e2b2f5
ProbablyRaging <probablyraging@gmail.com>
2025-04-06 02:28:06 +1100

    refactor(ui): add dynamic titles to the header

--- src/components/contexts/NavigationContext.tsx
+++ src/components/contexts/NavigationContext.tsx
@@ -1,18 +1,20 @@
 import { createContext, useContext, useState } from 'react';
 import type { ReactElement, ReactNode } from 'react';
 
-import type { NavigationContextType, ActivePageType, CurrentTabType } from '@/types';
+import type { NavigationContextType, ActivePageType, CurrentTabType, CurrentSettingsTabType } from '@/types';
 
 export const NavigationContext = createContext<NavigationContextType | undefined>(undefined);
 
 export const NavigationProvider = ({ children }: { children: ReactNode }): ReactElement => {
     const [activePage, setActivePage] = useState<ActivePageType>('games');
     const [currentTab, setCurrentTab] = useState<CurrentTabType>('achievements');
+    const [currentSettingsTab, setCurrentSettingsTab] = useState<CurrentSettingsTabType>('general');
 
     return (
         <NavigationContext.Provider value={{
             activePage, setActivePage,
             currentTab, setCurrentTab,
+            currentSettingsTab, setCurrentSettingsTab,
         }}>
             {children}
         </NavigationContext.Provider>
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,10 +3,13 @@ import type { Dispatch, SetStateAction } from 'react';
 export type CustomListType = 'cardFarmingList' | 'achievementUnlockerList' | 'autoIdleList' | 'favoritesList';
 export type ActivePageType = 'setup' | 'games' | 'idling' | 'freeGames' | 'settings' | `customlists/${string}`;
 export type CurrentTabType = 'achievements' | 'statistics';
+export type CurrentSettingsTabType = 'general' | 'card-farming' | 'achievement-unlocker' | 'logs';
 
 export interface NavigationContextType {
     activePage: ActivePageType;
     setActivePage: Dispatch<SetStateAction<ActivePageType>>;
     currentTab: CurrentTabType;
     setCurrentTab: Dispatch<SetStateAction<CurrentTabType>>;
+    currentSettingsTab: CurrentSettingsTabType;
+    setCurrentSettingsTab: Dispatch<SetStateAction<CurrentSettingsTabType>>;
 }
