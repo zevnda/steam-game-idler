4aa205d8a789a0f2669fe829b07a6271dd9f23ef
ProbablyRaging <probablyraging@gmail.com>
2025-04-07 14:13:26 +1000

    test lint-stages config

--- src/components/layout/Window.tsx
+++ src/components/layout/Window.tsx
@@ -8,6 +8,7 @@ import SteamWarning from '@/components/ui/SteamWarning'
 import useWindow from '@/hooks/layout/useWindow'
 
 const test = 'test';
+console.log(test);
 
 export default function Window(): ReactElement {
   const { userSummary } = useUserContext()
