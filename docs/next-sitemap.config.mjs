cc0853f5c05b1ffbc366c0e6f3948bb4154cbff4
zevnda <zevnda@gmail.com>
2025-06-25 02:25:47 +1000

    docs: fix sitemap error (#607)
    * docs: add comparison routes
    
    * docs: fix sitemap error

--- docs/next-sitemap.config.mjs
+++ docs/next-sitemap.config.mjs
@@ -1,6 +1,6 @@
 export default {
   siteUrl: 'https://steamgameidler.vercel.app',
-  sitemapBaseFileName: 'sitemap3',
+  sitemapBaseFileName: 'sitemap',
   changefreq: 'daily',
   priority: 0.5,
   generateIndexSitemap: false,
