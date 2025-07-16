feb20358edb12785d382ec22267547707e37c56e
zevnda <zevnda@gmail.com>
2025-07-16 12:41:58 +1000

    docs/improve settings documentation (#725)

--- docs/app/components/Screenshot.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSX, ReactElement } from 'react'
-
-import { cloneElement } from 'react'
-
-export function Screenshot({ children }: { children: JSX.Element }): ReactElement {
-  return cloneElement(children, {
-    className: 'mt-6 rounded-lg drop-shadow-md border border-[#242424]',
-  })
-}
