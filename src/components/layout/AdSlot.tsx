97f02ded0df6e76cf7309cd386f55ee98fa7df07
zevnda <zevnda@gmail.com>
2025-10-04 20:15:44 +1000

    refactor: improve ad delivery

--- src/components/layout/AdSlot.tsx
+++ src/components/layout/AdSlot.tsx
@@ -1,7 +1,6 @@
 import type { ReactElement } from 'react'
 
 import { cn, Spinner } from '@heroui/react'
-import { useCallback, useEffect, useState } from 'react'
 
 import { useNavigationContext } from '@/components/contexts/NavigationContext'
 import { useStateContext } from '@/components/contexts/StateContext'
@@ -9,24 +8,6 @@ import { useStateContext } from '@/components/contexts/StateContext'
 export default function AdSlot(): ReactElement {
   const { sidebarCollapsed } = useStateContext()
   const { activePage } = useNavigationContext()
-  const [iframeKey, setIframeKey] = useState(0)
-
-  const refreshIframe = useCallback(() => {
-    setIframeKey(prev => prev + 1)
-  }, [])
-
-  useEffect(() => {
-    const scheduleNextRefresh = (): NodeJS.Timeout => {
-      return setTimeout(() => {
-        refreshIframe()
-        scheduleNextRefresh()
-      }, 30000)
-    }
-
-    const timeoutId = scheduleNextRefresh()
-
-    return () => clearTimeout(timeoutId)
-  }, [refreshIframe])
 
   return (
     <div
@@ -38,13 +19,12 @@ export default function AdSlot(): ReactElement {
     >
       <div className='relative flex justify-center items-center overflow-hidden rounded-lg'>
         <iframe
-          key={iframeKey}
           className='overflow-scroll rounded-lg -mt-[351px] -ml-[300px] z-[1]'
           src='https://steamgameidler.com/ad-page'
           width='600'
           height='600'
           title='External Website'
-          sandbox='allow-scripts allow-same-origin allow-forms allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+          sandbox='allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation-by-user-activation'
         />
         <Spinner className='absolute inset-0 m-auto z-[0]' />
       </div>
