10ab748d1e6613629011d873532546d6a7484719
zevnda <zevnda@gmail.com>
2025-10-04 17:56:16 +1000

    docs: fix ad slots not loading (#852)

--- docs/app/components/AdOverlay.tsx
+++ docs/app/components/AdOverlay.tsx
@@ -8,18 +8,23 @@ export default function AdOverlay() {
 
   useEffect(() => {
     try {
-      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+      const adElements = document.querySelectorAll('.adsbygoogle')
+      adElements.forEach(() => {
+        ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+      })
     } catch (err) {
       console.error('AdSense error:', err)
     }
   }, [pathname])
+
+  console.log(pathname)
 
   return (
     <div key={pathname}>
       {/* Desktop side ads */}
       <div className='hidden lg:flex fixed top-1/2 -translate-y-1/2 left-4 flex-col gap-4 z-50 pointer-events-none'>
         <ins
-          className='adsbygoogle block bg-[#121316] rounded-lg'
+          className='adsbygoogle block rounded-lg'
           data-ad-client='ca-pub-8915288433444527'
           data-ad-slot='9143494556'
           style={{ width: '160px', height: '600px' }}
@@ -28,7 +33,7 @@ export default function AdOverlay() {
 
       <div className='hidden lg:flex fixed top-1/2 -translate-y-1/2 right-4 flex-col gap-4 z-50 pointer-events-none'>
         <ins
-          className='adsbygoogle block bg-[#121316] rounded-lg'
+          className='adsbygoogle block rounded-lg'
           data-ad-client='ca-pub-8915288433444527'
           data-ad-slot='3005445709'
           style={{ width: '160px', height: '600px' }}
@@ -36,14 +41,16 @@ export default function AdOverlay() {
       </div>
 
       {/* Mobile/Tablet banner ad */}
-      <div className='lg:hidden fixed bottom-4 left-1/2 -translate-x-1/2 z-50 pointer-events-none'>
-        <ins
-          className='adsbygoogle block bg-[#121316] rounded-lg'
-          data-ad-client='ca-pub-8915288433444527'
-          data-ad-slot='1265004536'
-          style={{ width: '300px', height: '50px' }}
-        />
-      </div>
+      {pathname !== '/ad-page' && (
+        <div className='lg:hidden fixed bottom-4 left-1/2 -translate-x-1/2 z-50 pointer-events-none'>
+          <ins
+            className='adsbygoogle block rounded-lg'
+            data-ad-client='ca-pub-8915288433444527'
+            data-ad-slot='1265004536'
+            style={{ width: '300px', height: '50px' }}
+          />
+        </div>
+      )}
     </div>
   )
 }
