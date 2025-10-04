ed002abcfa6f02f54b5fdbacee1d430eb797954b
zevnda <zevnda@gmail.com>
2025-10-04 21:37:20 +1000

    docs: temp disable ad overlay

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -118,7 +118,7 @@ export default async function RootLayout({ children }: { children: ReactNode }):
         >
           {children}
         </Layout>
-        <AdOverlay />
+        {/* <AdOverlay /> */}
         <Analytics />
       </body>
     </html>
