8bfe80799e53d9c07b930b3048c23b9cff72bc99
zevnda <zevnda@gmail.com>
2025-11-05 09:15:50 +1100

    docs: remove speed insights

--- docs/app/components/TelemetryLoader.tsx
+++ docs/app/components/TelemetryLoader.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { usePathname } from 'next/navigation'
 
 export default function TelemetryLoader() {
@@ -14,7 +13,6 @@ export default function TelemetryLoader() {
   return (
     <>
       <Analytics />
-      <SpeedInsights />
     </>
   )
 }
