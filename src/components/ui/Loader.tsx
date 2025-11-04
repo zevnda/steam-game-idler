daa8c606a2c5a11a6c808024eb3ba0725960cf55
zevnda <zevnda@gmail.com>
2025-11-04 20:43:48 +1100

    refactor(chat): show spinner when loading messages (#978)

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -2,10 +2,16 @@ import type { ReactElement } from 'react'
 
 import { Spinner } from '@heroui/react'
 
-export default function Loader(): ReactElement {
+export default function Loader({ label, styles }: { label?: string; styles?: string }): ReactElement {
   return (
-    <div className='flex justify-center items-center w-calc h-calc'>
-      <Spinner variant='simple' />
+    <div className={`flex justify-center items-center w-calc h-calc ${styles}`}>
+      <Spinner
+        variant='simple'
+        label={label}
+        classNames={{
+          label: 'text-xs',
+        }}
+      />
     </div>
   )
 }
