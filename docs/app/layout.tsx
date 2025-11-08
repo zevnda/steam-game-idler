da69bf82aa3af3bb703162c53703eb3804cfb1cd
zevnda <zevnda@gmail.com>
2025-11-09 06:37:03 +1100

    docs: temporarily disable telemetry (#1012)
    * docs: temporarily disable telemetry
    
    * comment out unused import

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -10,7 +10,8 @@ import 'nextra-theme-docs/style.css'
 import './globals.css'
 
 import AdOverlay from '@docs/components/AdOverlay'
-import TelemetryLoader from '@docs/components/TelemetryLoader'
+
+// import TelemetryLoader from '@docs/components/TelemetryLoader'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -157,7 +158,7 @@ export default async function RootLayout({ children }: { children: ReactNode }):
           {children}
         </Layout>
         <AdOverlay />
-        <TelemetryLoader />
+        {/* <TelemetryLoader /> */}
       </body>
     </html>
   )
