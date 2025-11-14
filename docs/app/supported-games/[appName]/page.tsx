c00f3fef4fb2936521ad8c017d1e84316fdf2f94
zevnda <zevnda@gmail.com>
2025-11-14 21:58:06 +1100

    docs: add adsterra ad componenets (#1055)

--- docs/app/supported-games/[appName]/page.tsx
+++ docs/app/supported-games/[appName]/page.tsx
@@ -1,7 +1,10 @@
 import type { ReactElement } from 'react'
 
 import AdComponent from './AdComponent'
+import AdComponentFour from '@docs/supported-games/[appName]/AdComponentFour'
+import AdComponentThree from '@docs/supported-games/[appName]/AdComponentThree'
 import AdComponentTwo from '@docs/supported-games/[appName]/AdComponentTwo'
+import Script from 'next/script'
 
 interface PageProps {
   params: {
@@ -414,6 +417,12 @@ export default async function AdPage({ params }: PageProps): Promise<ReactElemen
       </div>
       <AdComponent />
       <AdComponentTwo />
+      <AdComponentThree />
+      <AdComponentFour />
+      <Script
+        src='//pl28051196.effectivegatecpm.com/c4/26/a5/c426a5dd2fa7302020e196b721701d7e.js'
+        strategy='afterInteractive'
+      />
     </div>
   )
 }
