2e76a91a0a469a308c01277ac4ad7d626c94ad63
zevnda <zevnda@gmail.com>
2025-11-27 11:59:44 +1100

    refactor(ui): update pro badge and button to match new ui

--- src/components/ui/ProBadge.tsx
+++ src/components/ui/ProBadge.tsx
@@ -2,15 +2,23 @@ import type { ReactElement } from 'react'
 
 import { cn } from '@heroui/react'
 
+import { useUserContext } from '@/components/contexts/UserContext'
+
 export default function ProBadge({ className }: { className?: string }): ReactElement {
+  const { isPro } = useUserContext()
+
   return (
     <span
       className={cn('inline-block rounded-full px-2 ml-1 font-black text-white italic', className)}
       style={{
-        backgroundImage: 'linear-gradient(120deg, #700084ff 0%, #7e15ffff 40%, #0095ffff 85%, #00a2c3ff 100%)',
+        backgroundImage: isPro
+          ? 'linear-gradient(90deg, hsl(var(--heroui-dynamic) / 0.2) 0%, hsl(var(--heroui-dynamic) / 0.8) 100%)'
+          : 'linear-gradient(90deg, #29460aff 0%, #5f9723ff 40%, #77b832ff 70%, #a5f84cff 100%)',
       }}
     >
       PRO
     </span>
   )
 }
+
+// #29460aff 0%, #5f9723ff 40%, #77b832ff 70%, #a5f84cff 100%
