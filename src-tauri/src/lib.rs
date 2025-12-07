34ee28904cadae20e4c25bd253c2d5f10d1b6eae
zevnda <zevnda@gmail.com>
2025-12-07 16:37:08 +1100

    feat(ui): add context menu to allow copy and paste (#1325)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -62,6 +62,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin({
             let mut builder = tauri_plugin_window_state::Builder::new().with_state_flags(
                 StateFlags::SIZE
