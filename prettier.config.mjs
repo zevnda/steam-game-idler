f5d2a966ed260b80873cd5a84bb3ed6173090997
Autapomorph <Autapomorph@users.noreply.github.com>
2026-01-28 19:53:49 +0300

    revisit prettier settings

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,37 +1,19 @@
-/** @type {import("prettier").Config} */
-const config = {
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
   tabWidth: 2,
   useTabs: false,
   semi: false,
-  printWidth: 120,
+  printWidth: 100,
   singleQuote: true,
   jsxSingleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
   quoteProps: 'consistent',
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
-  importOrder: [
-    '<TYPES>^(node:)',
-    '<TYPES>',
-    '<TYPES>^[.]',
-    '',
-    '@internationalized/(.*)$',
-    '',
-    '@tauri-apps/(.*)$',
-    '',
-    '^@heroui/react$',
-    '^(react/(.*)$)|^(react$)',
-    '<THIRD_PARTY_MODULES>',
-    '',
-    '^@/components/(.*)$',
-    '^@/hooks/(.*)$',
-    '^@/utils/(.*)$',
-    '',
-    '@/i18n/(.*)$',
-  ],
-  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
-  importOrderTypeScriptVersion: '5.0.0',
-  importOrderCaseSensitive: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  objectWrap: 'preserve',
+  endOfLine: 'lf',
 }
-
-export default config
