fbbcf97b9d23b0e6ccb0ae13c5e78568f7f216a6
zevnda <zevnda@gmail.com>
2026-03-03 13:04:31 +1100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   devIndicators: false,
-  transpilePackages: ['geist', 'pusher-js'],
+  transpilePackages: ['geist'],
   images: {
     unoptimized: true,
     remotePatterns: [
