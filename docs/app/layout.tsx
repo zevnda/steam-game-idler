609ec615141bc247239a37c5484e0d91e5a4472d
zevnda <zevnda@gmail.com>
2026-06-15 23:56:58 +1000

    docs: add google analytics (#1740)

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -160,6 +160,20 @@ export default function Layout({ children }: LayoutProps) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
         />
 
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-W2GWCP59BN'
+          strategy='afterInteractive'
+        />
+
+        <Script id='ga-init' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W2GWCP59BN');
+          `}
+        </Script>
+
         <script
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8915288433444527'
