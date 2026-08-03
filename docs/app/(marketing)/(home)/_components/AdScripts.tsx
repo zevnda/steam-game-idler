73799cdf33a0a645bfbccf3b6b5eb0f794b2c42f
zevnda <zevnda@gmail.com>
2026-08-03 21:07:02 +1000

    docs: revert ad loading hydration fix (#2041)

--- docs/app/(marketing)/(home)/_components/AdScripts.tsx
+++ docs/app/(marketing)/(home)/_components/AdScripts.tsx
@@ -1,9 +1,12 @@
+import Script from 'next/script'
+
 export default function AdScripts() {
   return (
-    <script
+    <Script
       async
       src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8915288433444527'
       crossOrigin='anonymous'
+      strategy='lazyOnload'
     />
   )
 }
